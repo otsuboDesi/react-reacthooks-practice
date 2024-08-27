74f7d19ad008d9ab74cb3cd00edbc1fe82d489a4
otsuboDesi <otsubodesignare@gmail.com>
2024-08-27 15:21:27 +0900

    Edit: コンポーネントで切り分けする

--- src/App.jsx
+++ src/App.jsx
@@ -25,17 +25,24 @@ const LoginControl = () => {
   return <LoginButton toggleIsLoggedIn={toggledIsLoggedIn} />;
 };
 
-const App = () => {
+const IfContainer = () => {
   const isReact = true;
   return (
-    <>
+    <div style={{ background: "lightgray", padding: 20, color: "gray" }}>
       <p>{isReact ? "Hello React!!!1" : "Hello!"}</p>
       {/* isReact === true */}
       {isReact && <p>Hello React!!!2</p>}
       {/* isReact === false */}
       <p>{isReact || "Hello React!!!3"}</p>
+    </div>
+  );
+};
+const App = () => {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: 20 }}>
       <LoginControl />
-    </>
+      <IfContainer />
+    </div>
   );
 };
 
